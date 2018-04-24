d8a827e328e27b96bced8c3105e5b0203160e7d2
Paul-Sirawit Kerebel <psk@Paul-Sirawits-MacBook-Pro.local>
2018-04-25 01:43:39 +0900

    account create unittest

--- test/account.py
+++ test/account.py
@@ -1,33 +1,115 @@
 import unittest
 import sys
 import json
+
 sys.path.insert(0,'..')
 from api import neoapi
+from config.database import db_session
+from models.User import User as UserModel
 
 class AccountCreate(unittest.TestCase):
+    def setUp(self):
+        neo = neoapi()
+        self.api = neo.activate_testing()
 
     def test_valide_request(self):
-        neo = neoapi()
-        test = neo.activate_testing()
-        todo = {
-                "email": "test@test.com",
-                "last_name": "Last Name",
-                "password": "VerySecurePassword",
-                "first_name": "First Name",
-                "birthday": "1995-12-25"
-                }
-        vr = test.post('/account/create', data=json.dumps(todo), content_type='application/json')
-        print(vr)
+        json_data = {
+            "email": "test@test.com",
+            "last_name": "Last Name",
+            "password": "VerySecurePassword",
+            "first_name": "First Name",
+            "birthday": "1995-12-25"
+        }
+        response = self.api.post('/account/create', data=json.dumps(json_data), content_type='application/json')
+        response_json = json.loads(response.data)
+        assert response.status_code == 201
+        assert response_json['success'] == True
 
-    def test_no_json(self):
-        neo = neoapi()
-        test = neo.activate_testing()
-        todo = {
-                "email": "test@test.com",
-                "last_name": "Last Name",
-                "password": "VerySecurePassword",
-                "first_name": "First Name",
-                "birthday": "1995-12-25"
-                }
-        vr = test.post('/account/create', data=json.dumps(todo), content_type='application/json')
-        print(vr)
+    def test_empty_json(self):
+        todo = {}
+        response = self.api.post('/account/create', data=json.dumps(todo), content_type='application/json')
+        response_json = json.loads(response.data)
+        assert response.status_code == 409
+        assert response_json['success'] == False
+
+    def test_missing_email(self):
+        json_data = {
+            "last_name": "Last Name",
+            "password": "VerySecurePassword",
+            "first_name": "First Name",
+            "birthday": "1995-12-25"
+        }
+        response = self.api.post('/account/create', data=json.dumps(json_data), content_type='application/json')
+        response_json = json.loads(response.data)
+        assert response.status_code == 409
+        assert response_json['success'] == False
+
+    def test_missing_last_name(self):
+        json_data = {
+            "email": "test@test.com",
+            "password": "VerySecurePassword",
+            "first_name": "First Name",
+            "birthday": "1995-12-25"
+        }
+        response = self.api.post('/account/create', data=json.dumps(json_data), content_type='application/json')
+        response_json = json.loads(response.data)
+        assert response.status_code == 409
+        assert response_json['success'] == False
+
+    def test_missing_password(self):
+        json_data = {
+            "email": "test@test.com",
+            "last_name": "Last Name",
+            "first_name": "First Name",
+            "birthday": "1995-12-25"
+        }
+        response = self.api.post('/account/create', data=json.dumps(json_data), content_type='application/json')
+        response_json = json.loads(response.data)
+        assert response.status_code == 409
+        assert response_json['success'] == False
+
+    def test_missing_first_name(self):
+        json_data = {
+            "email": "test@test.com",
+            "last_name": "Last Name",
+            "password": "VerySecurePassword",
+            "birthday": "1995-12-25"
+        }
+        response = self.api.post('/account/create', data=json.dumps(json_data), content_type='application/json')
+        response_json = json.loads(response.data)
+        assert response.status_code == 409
+        assert response_json['success'] == False
+
+    def test_missing_birthday(self):
+        json_data = {
+            "email": "test@test.com",
+            "last_name": "Last Name",
+            "password": "VerySecurePassword",
+            "first_name": "First Name"
+        }
+        response = self.api.post('/account/create', data=json.dumps(json_data), content_type='application/json')
+        response_json = json.loads(response.data)
+        assert response.status_code == 409
+        assert response_json['success'] == False
+
+    def test_used_mail(self):
+        new_user = UserModel(
+            email="clone@gmail.com",
+            password="password",
+            first_name="first_name",
+            last_name="last_name",
+            birthday="1999-02-02"
+            )
+        db_session.add(new_user)
+        db_session.commit()
+        json_data = {
+            "email": "clone@gmail.com",
+            "last_name": "Last Name",
+            "password": "VerySecurePassword",
+            "first_name": "First Name",
+            "birthday": "1995-12-25"
+        }
+        response = self.api.post('/account/create', data=json.dumps(json_data), content_type='application/json')
+        response_json = json.loads(response.data)
+        assert response.status_code == 409
+        assert response_json['success'] == False
