6b1550411844a9361c8b7807afb3e009f7897e80
Paul-Sirawit Kerebel <psk@Paul-Sirawits-MacBook-Pro.local>
2018-05-14 02:10:09 +0900

    test message template

--- routes/Facebook.py
+++ routes/Facebook.py
@@ -67,23 +67,9 @@ def SendMessage(recipient_id, message_text):
         "recipient": {
             "id": recipient_id
         },
-        "message":{
-            "attachment":{
-            "type":"template",
-            "payload":{
-                "template_type":"receipt",
-                "recipient_name":"Stephane Crozatier",
-                "order_number":"12345678902",
-                "currency":"USD",
-                "payment_method":"Visa 2345",        
-                "order_url":"http://petersapparel.parseapp.com/order?order_id=123456",
-                "timestamp":"1428444852",         
-            }
-            }
+        "message": {
+            "text": message_text
         }
-        #"message": {
-        #    "text": message_text
-        #}
     })
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
     if r.status_code != 200:
