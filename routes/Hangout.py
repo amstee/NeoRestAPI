9175fd394d78c29d5ad68669b77e3a8d7c52f666
Paul-Sirawit Kerebel <psk@Paul-Sirawits-MacBook-Pro.local>
2018-07-13 04:10:34 +0200

    Add hangout basic test

---
+++ routes/Hangout.py
@@ -0,0 +1,48 @@
+from flask_restful import Resource
+from flask.views import MethodView
+from config.database import db_session
+from models.User import User
+from models.Circle import Circle
+from models.UserToCircle import UserToCircle
+from models.Conversation import Conversation
+from models.UserToConversation import UserToConversation
+from models.Message import Message
+from utils.decorators import securedRoute, checkContent, securedAdminRoute, securedDeviceRoute
+from utils.contentChecker import contentChecker
+from utils.apiUtils import *
+from webargs import fields, validate
+from webargs.flaskparser import use_args, use_kwargs, parser, abort
+import requests
+import sys
+import hashlib
+import jwt
+import datetime
+import json
+
+TOKEN="yZZieXB8D64T1qMxI9fJVCgC1vVMUB70PB9p3lIYSN4="
+
+def isTokenValid(content):
+    try:
+        if content['token'] == TOKEN:
+            return True
+        return False
+    except Exception as e:
+        print(e, file=sys.stderr)
+        return False
+
+class Webhook(Resource):
+    @checkContent
+    def post(self, content):
+        try:
+            if isTokenValid(content) == True:
+                if content['type'] == 'ADDED_TO_SPACE' and content['space']['type'] == 'ROOM':
+                    text = 'Thanks for adding me to "%s"!' % content['space']['displayName']
+                elif content['type'] == 'MESSAGE':
+                    text = 'You said: `%s`' % content['message']['text']
+                else:
+                    return
+                return json.jsonify({'text': text})
+            return
+        except Exception as e:
+            print(e, file=sys.stderr)
+            return
