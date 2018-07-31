08be7505ab167fa70500a89171c8479bcbd3a57e
jeremy <jeremy.barneron@gmail.com>
2018-07-31 11:06:12 +0200

    Created first webrtc signaling handlers

--- sockets/webrtc.py
+++ sockets/webrtc.py
@@ -1,35 +1,47 @@
 from config import socketio
 from config.sockets import sockets
-from config.turnserver import SECRET_KEY
+from config.webrtc import STUN, TURN
 from flask import request
 from flask_socketio import emit
-from utils.contentChecker import check_json
-import time
-import hmac
-import hashlib
+from utils.websocket import get_dest
 
 
 @socketio.on('webrtc_credentials')
-def turn_auth(json):
+def turn_auth():
     sid = request.sid
     try:
         socket = sockets.find_socket(sid)
         if socket is None or socket.authenticated is False:
             emit('error', 'Socket user introuvable', room=sid, namespace='/')
         else:
-            unix_time = time.time()
-            username = str(unix_time) + ':' + str(socket.client.id) + str(socket.is_device)
-            digest_maker = hmac.new(SECRET_KEY, '', hashlib.sha1)
-            digest_maker.update(username)
-            password = digest_maker.digest()
-            emit('webrtc_credentials', {username: username, password: password}, room=sid, namespace='/')
+            username, password = socket.generate_credentials()
+            emit('webrtc_config', {"username": username, "password": password}, room=sid, namespace='/')
     except Exception as e:
         emit("error", str(e), room=sid, namespace='/')
 
 
 @socketio.on('webrtc_forward')
 def forward(json):
-    pass
+    sid = request.sid
+    try:
+        socket = sockets.find_socket(sid)
+        if socket is None or socket.authenticated is False:
+            emit('error', "Socket user introuvable", room=sid, namespace='/')
+        else:
+            is_device, dest = get_dest(json)
+            if dest is None:
+                raise Exception("Utilisateur introuvable")
+            dest_socket = sockets.find_user(dest, is_device)
+            if dest_socket is None:
+                raise Exception("Destinataire non connecte aux websockets")
+            dest_socket.emit("webrtc_forward", {
+                "sender_id": socket.client.id,
+                "is_device": socket.client.is_device,
+                "content": json
+            })
+            socket.emit("success", "Message forwarded")
+    except Exception as e:
+        emit("error", str(e), room=sid, namespace='/')
 
 
 @socketio.on('webrtc_join_room')
@@ -48,10 +60,10 @@ def room_forward(json):
 
 
 @socketio.on('webrtc_stun_info')
-def stun_info(json):
-    pass
+def stun_info():
+    emit("webrtc_config", STUN, room=request.sid, namespace='/')
 
 
 @socketio.on('webrtc_turn_info')
-def turn_info(json):
-    pass
+def turn_info():
+    emit("webrtc_config", TURN, room=request.sid, namespace='/')
