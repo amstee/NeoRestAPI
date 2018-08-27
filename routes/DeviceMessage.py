e5420d6475e9f478dfe49034ed588eb239f795b9
Kerebel Paul <kerebelpaul@Kerebels-MacBook-Pro.local>
2018-08-27 18:13:53 +0200

    basic response log DeviceMessage.py

--- routes/DeviceMessage.py
+++ routes/DeviceMessage.py
@@ -21,48 +21,53 @@ class DeviceMessageCreate(Resource):
     @check_content
     @secured_admin_route
     def post(self, content, admin):
-        logger.info("[%s] [%s] [%s] [%s] [%s]",
-                    request.method, request.host, request.path, request.content_type, request.data)
         try:
             content_checker("files", "device_id", "conversation_id", "text", "directory_name")
             device = db.session.query(Device).filter(Device.id == content["device_id"]).first()
             if device is None:
-                return FAILED("Le device spécifié est introuvable")
-            conversation = db.session.query(Conversation).filter(Conversation.id == content["conversation_id"]).first()
-            if conversation is None:
-                return FAILED("La conversation spécifié est introuvable")
-            message = Message(content=content["text"], is_user=False)
-            message.conversation = conversation
-            message.device = device
-            media_list = []
-            if "files" in content:
-                for file in content["files"]:
-                    media = Media()
-                    media.identifier = file
-                    MessageToMedia(message=message, media=media)
-                    db.session.commit()
-                    media_list.append(media.get_simple_content())
-            db.session.commit()
-            if not media_list:
-                emit('message', {
-                    'conversation_id': conversation.id,
-                    'message': message.get_simple_json_compliant_content(),
-                    'time': message.sent.isoformat(),
-                    'sender': {"email": "ADMIN"},
-                    'media_list': media_list,
-                    'status': 'done'},
-                     room='conversation_' + str(conversation.id), namespace='/')
+                resp = FAILED("Le device spécifié est introuvable")
             else:
-                emit('message', {'conversation_id': conversation.id, 'message':
-                                 message.get_simple_json_compliant_content(),
-                                 'status': 'pending'}, room='conversation_' + str(conversation.id), namespace='/')
-            info_sender = "[" + conversation.name + "] " + device.name + " : "
-            try:
-                messenger_conversation_model_send(0, conversation, info_sender + message.text_content)
-            except Exception:
-                pass
-            resp = jsonify({"success": True, 'media_list': media_list, 'message_id': message.id})
-            resp.status_code = 200
+                conversation = db.session.query(Conversation).filter(Conversation.id ==
+                                                                     content["conversation_id"]).first()
+                if conversation is None:
+                    resp = FAILED("La conversation spécifié est introuvable")
+                else:
+                    message = Message(content=content["text"], is_user=False)
+                    message.conversation = conversation
+                    message.device = device
+                    media_list = []
+                    if "files" in content:
+                        for file in content["files"]:
+                            media = Media()
+                            media.identifier = file
+                            MessageToMedia(message=message, media=media)
+                            db.session.commit()
+                            media_list.append(media.get_simple_content())
+                    db.session.commit()
+                    if not media_list:
+                        emit('message', {
+                            'conversation_id': conversation.id,
+                            'message': message.get_simple_json_compliant_content(),
+                            'time': message.sent.isoformat(),
+                            'sender': {"email": "ADMIN"},
+                            'media_list': media_list,
+                            'status': 'done'},
+                             room='conversation_' + str(conversation.id), namespace='/')
+                    else:
+                        emit('message', {'conversation_id': conversation.id, 'message':
+                                         message.get_simple_json_compliant_content(),
+                                         'status': 'pending'}, room='conversation_' + str(conversation.id),
+                             namespace='/')
+                    info_sender = "[" + conversation.name + "] " + device.name + " : "
+                    try:
+                        messenger_conversation_model_send(0, conversation, info_sender + message.text_content)
+                    except Exception:
+                        pass
+                    resp = jsonify({"success": True, 'media_list': media_list, 'message_id': message.id})
+                    resp.status_code = 200
+            logger.info("[%s] [%s] [%s] [%s] [%s] [%d]",
+                        request.method, request.host, request.path,
+                        request.content_type, request.data, resp.status_code)
             return resp
         except Exception as e:
             return FAILED(e)
@@ -72,22 +77,25 @@ class DeviceMessageDelete(Resource):
     @check_content
     @secured_route
     def post(self, content, device):
-        logger.info("[%s] [%s] [%s] [%s] [%s]",
-                    request.method, request.host, request.path, request.content_type, request.data)
         try:
             content_checker("message_id")
             message = db.session.query(Message).filter(Message.id == content["message_id"]).first()
             if message is None:
-                return FAILED("Message spécifié introuvable")
-            if message.is_user or message.device_id != device.id:
-                return FAILED("Ce message n'appartient pas a ce device", 403)
-            id_message = message.id
-            conv_id = message.conversation_id
-            db.session.delete(message)
-            db.session.commit()
-            emit('message', {"conversation_id": conv_id, "message_id": id_message, "event": 'delete'},
-                 room="conversation_" + str(conv_id), namespace='/')
-            return SUCCESS()
+                resp = FAILED("Message spécifié introuvable")
+            elif message.is_user or message.device_id != device.id:
+                resp = FAILED("Ce message n'appartient pas a ce device", 403)
+            else:
+                id_message = message.id
+                conv_id = message.conversation_id
+                db.session.delete(message)
+                db.session.commit()
+                emit('message', {"conversation_id": conv_id, "message_id": id_message, "event": 'delete'},
+                     room="conversation_" + str(conv_id), namespace='/')
+                resp = SUCCESS()
+            logger.info("[%s] [%s] [%s] [%s] [%s] [%d]",
+                        request.method, request.host, request.path,
+                        request.content_type, request.data, resp.status_code)
+            return resp
         except Exception as e:
             return FAILED(e)
 
@@ -96,16 +104,19 @@ class DeviceMessageInfo(Resource):
     @check_content
     @secured_route
     def post(self, content, device):
-        logger.info("[%s] [%s] [%s] [%s] [%s]",
-                    request.method, request.host, request.path, request.content_type, request.data)
         try:
             content_checker("message_id")
             message = db.session.query(Message).filter(Message.id == content["message_id"]).first()
             if message is None:
-                return FAILED("Message spécifié introuvable")
-            if not device_has_access_to_message(message, device):
-                return FAILED("Ce device ne peut pas voir ce message", 403)
-            return jsonify({"success": True, "content": message.get_content()})
+                resp = FAILED("Message spécifié introuvable")
+            elif not device_has_access_to_message(message, device):
+                resp = FAILED("Ce device ne peut pas voir ce message", 403)
+            else:
+                resp = jsonify({"success": True, "content": message.get_content()})
+            logger.info("[%s] [%s] [%s] [%s] [%s] [%d]",
+                        request.method, request.host, request.path,
+                        request.content_type, request.data, resp.status_code)
+            return resp
         except Exception as e:
             return FAILED(e)
 
@@ -114,16 +125,19 @@ class DeviceMessageList(Resource):
     @check_content
     @secured_route
     def post(self, content, device):
-        logger.info("[%s] [%s] [%s] [%s] [%s]",
-                    request.method, request.host, request.path, request.content_type, request.data)
         try:
             content_checker("conversation_id", "quantity")
             conv = db.session.query(Conversation).filter(Conversation.id == content["conversation_id"]).first()
             if conv.device_access is False or conv.circle.device.id != device.id:
-                return FAILED("Ce device ne peut pas voir cette conversation", 403)
-            messages = db.session.query(Message).filter(Message.conversation_id == conv.id).\
-                limit(content["quantity"]).all()
-            return jsonify({"success": True, "content": [message.get_content() for message in messages]})
+                resp = FAILED("Ce device ne peut pas voir cette conversation", 403)
+            else:
+                messages = db.session.query(Message).filter(Message.conversation_id == conv.id).\
+                    limit(content["quantity"]).all()
+                resp = jsonify({"success": True, "content": [message.get_content() for message in messages]})
+            logger.info("[%s] [%s] [%s] [%s] [%s] [%d]",
+                        request.method, request.host, request.path,
+                        request.content_type, request.data, resp.status_code)
+            return resp
         except Exception as e:
             return FAILED(e)
 
@@ -132,20 +146,23 @@ class DeviceMessageUpdate(Resource):
     @check_content
     @secured_route
     def post(self, content, device):
-        logger.info("[%s] [%s] [%s] [%s] [%s]",
-                    request.method, request.host, request.path, request.content_type, request.data)
         try:
             content_checker("message_id")
             message = db.session.query(Message).filter(Message.id == content["message_id"]).first()
             if message is None:
-                return FAILED("Message spécifié introuvable")
-            if not device_has_access_to_message(message, device):
-                return FAILED("Ce device n'a pas accès a ce message", 403)
-            message.update_content(sent=content["sent"] if "sent" in content else None,
-                                   read=content["read"] if "read" in content else None,
-                                   content=content["text_message"] if "text_message" in content else None)
-            emit('message', {"conversation_id": message.conversation_id, "message_id": message.id, "event": 'update'},
-                 room="conversation_" + str(message.conversation_id), namespace='/')
-            return SUCCESS()
+                resp = FAILED("Message spécifié introuvable")
+            elif not device_has_access_to_message(message, device):
+                resp = FAILED("Ce device n'a pas accès a ce message", 403)
+            else:
+                message.update_content(sent=content["sent"] if "sent" in content else None,
+                                       read=content["read"] if "read" in content else None,
+                                       content=content["text_message"] if "text_message" in content else None)
+                emit('message', {"conversation_id": message.conversation_id, "message_id": message.id, "event": 'update'},
+                     room="conversation_" + str(message.conversation_id), namespace='/')
+                resp = SUCCESS()
+            logger.info("[%s] [%s] [%s] [%s] [%s] [%d]",
+                        request.method, request.host, request.path,
+                        request.content_type, request.data, resp.status_code)
+            return resp
         except Exception as e:
             return FAILED(e)
