da45201656b4c19e872daffa4a1feb7400052f47
Kerebel Paul <kerebelpaul@Kerebels-MacBook-Pro.local>
2018-08-27 18:16:18 +0200

    basic response log DeviceMessageLogic.py

--- routes/DeviceMessageLogic.py
+++ routes/DeviceMessageLogic.py
@@ -22,43 +22,45 @@ class FirstDeviceMessageSend(Resource):
     @check_content
     @secured_route
     def post(self, content, device):
-        logger.info("[%s] [%s] [%s] [%s] [%s]",
-                    request.method, request.host, request.path, request.content_type, request.data)
         try:
             content_checker("email")
             user = db.session.query(UserModel).filter(UserModel.email == content["email"]).first()
             if user is None:
-                return FAILED("Utilisateur spécifié introuvable")
-            if not device.circle.has_member(user):
-                return FAILED("L'utilisateur n'est pas dans votre cercle")
-            conversation = Conversation(device_access=True)
-            conversation.circle = device.circle
-            conversation.name = "NewConversation"
-            link = UserToConversation(privilege="ADMIN")
-            link.user = user
-            link.conversation = conversation
-            message = Message(content=content["text_message"] if "text_message" in content else "", is_user=False)
-            message.conversation = conversation
-            message.device = device
-            media_list = []
-            if "files" in content:
-                for file in content["files"]:
-                    media = Media()
-                    media.identifier = file
-                    MessageToMedia(message=message, media=media)
-                    db.session.commit()
-                    media_list.append(media.get_simple_content())
-            db.session.commit()
-            sockets.notify_user(user, False, 'conversation',
-                                {"conversation_id": conversation.id,
-                                 "event": 'invite'})
-            info_and_message = "[" + conversation.name + "] " + device.name + " : " + str(message.text_content)
-            try:
-                messenger_user_model_send(user_target=user, text_message=info_and_message)
-            except Exception:
-                pass
-            resp = jsonify({"success": True, 'media_list': media_list, 'message_id': message.id})
-            resp.status_code = 200
+                resp = FAILED("Utilisateur spécifié introuvable")
+            elif not device.circle.has_member(user):
+                resp = FAILED("L'utilisateur n'est pas dans votre cercle")
+            else:
+                conversation = Conversation(device_access=True)
+                conversation.circle = device.circle
+                conversation.name = "NewConversation"
+                link = UserToConversation(privilege="ADMIN")
+                link.user = user
+                link.conversation = conversation
+                message = Message(content=content["text_message"] if "text_message" in content else "", is_user=False)
+                message.conversation = conversation
+                message.device = device
+                media_list = []
+                if "files" in content:
+                    for file in content["files"]:
+                        media = Media()
+                        media.identifier = file
+                        MessageToMedia(message=message, media=media)
+                        db.session.commit()
+                        media_list.append(media.get_simple_content())
+                db.session.commit()
+                sockets.notify_user(user, False, 'conversation',
+                                    {"conversation_id": conversation.id,
+                                     "event": 'invite'})
+                info_and_message = "[" + conversation.name + "] " + device.name + " : " + str(message.text_content)
+                try:
+                    messenger_user_model_send(user_target=user, text_message=info_and_message)
+                except Exception:
+                    pass
+                resp = jsonify({"success": True, 'media_list': media_list, 'message_id': message.id})
+                resp.status_code = 200
+            logger.info("[%s] [%s] [%s] [%s] [%s] [%d]",
+                        request.method, request.host, request.path,
+                        request.content_type, request.data, resp.status_code)
             return resp
         except Exception as e:
             return FAILED(e)
@@ -68,47 +70,49 @@ class DeviceMessageSend(Resource):
     @check_content
     @secured_route
     def post(self, content, device):
-        logger.info("[%s] [%s] [%s] [%s] [%s]",
-                    request.method, request.host, request.path, request.content_type, request.data)
         try:
             content_checker("conversation_id")
             conv = db.session.query(Conversation).filter(Conversation.id == content["conversation_id"]).first()
             if conv is None:
-                return FAILED("Conversation introuvable")
+                resp = FAILED("Conversation introuvable")
             if conv.device_access is False or conv.circle.id != device.circle.id:
-                return FAILED("Vous ne pouvez pas acceder a cette conversation", 403)
-            message = Message(content=content["text_message"] if "text_message" in content else "", is_user=False)
-            message.conversation = conv
-            message.device = device
-            media_list = []
-            if "files" in content:
-                for file in content["files"]:
-                    media = Media()
-                    media.identifier = file
-                    MessageToMedia(message=message, media=media)
-                    db.session.commit()
-                    media_list.append(media.get_simple_content())
-            db.session.commit()
-            if not media_list:
-                emit('message', {
-                    'conversation_id': message.conversation_id,
-                    'message': message.get_simple_json_compliant_content(),
-                    'time': message.sent.isoformat(),
-                    'sender': device.get_simple_json_compliant_content(),
-                    'media_list': media_list,
-                    'status': 'done'},
-                     room='conversation_' + str(message.conversation_id), namespace='/')
+                resp = FAILED("Vous ne pouvez pas acceder a cette conversation", 403)
             else:
-                emit('message', {'conversation_id': message.conversation_id, 'message':
-                     message.get_simple_json_compliant_content(),
-                     'status': 'pending'}, room='conversation_' + str(message.conversation_id), namespace='/')
-            info_sender = "[" + conv.name + "] " + device.name + " : "
-            try:
-                messenger_conversation_model_send(0, conv, info_sender + message.text_content)
-            except Exception:
-                pass
-            resp = jsonify({"success": True, 'media_list': media_list, 'message_id': message.id})
-            resp.status_code = 200
+                message = Message(content=content["text_message"] if "text_message" in content else "", is_user=False)
+                message.conversation = conv
+                message.device = device
+                media_list = []
+                if "files" in content:
+                    for file in content["files"]:
+                        media = Media()
+                        media.identifier = file
+                        MessageToMedia(message=message, media=media)
+                        db.session.commit()
+                        media_list.append(media.get_simple_content())
+                db.session.commit()
+                if not media_list:
+                    emit('message', {
+                        'conversation_id': message.conversation_id,
+                        'message': message.get_simple_json_compliant_content(),
+                        'time': message.sent.isoformat(),
+                        'sender': device.get_simple_json_compliant_content(),
+                        'media_list': media_list,
+                        'status': 'done'},
+                         room='conversation_' + str(message.conversation_id), namespace='/')
+                else:
+                    emit('message', {'conversation_id': message.conversation_id, 'message':
+                         message.get_simple_json_compliant_content(),
+                         'status': 'pending'}, room='conversation_' + str(message.conversation_id), namespace='/')
+                info_sender = "[" + conv.name + "] " + device.name + " : "
+                try:
+                    messenger_conversation_model_send(0, conv, info_sender + message.text_content)
+                except Exception:
+                    pass
+                resp = jsonify({"success": True, 'media_list': media_list, 'message_id': message.id})
+                resp.status_code = 200
+            logger.info("[%s] [%s] [%s] [%s] [%s] [%d]",
+                        request.method, request.host, request.path,
+                        request.content_type, request.data, resp.status_code)
             return resp
         except Exception as e:
             return FAILED(e)
