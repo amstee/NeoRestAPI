65b1ff66457fba246a75a41c9b100702c7a8772c
Kerebel Paul <kerebelpaul@Kerebels-MacBook-Pro.local>
2018-08-29 21:46:40 +0200

    config log

--- config/log.py
+++ config/log.py
@@ -6,6 +6,15 @@ with open('config.json') as data_file:
 
 LOG_ACTIVATE = neo_config["logs"]["activate"]
 LOG_LEVEL = neo_config["logs"]["level"]
+LOG_FOLDER = neo_config["logs"]["folder"]
+LOG_FILE = neo_config["logs"]["file"]
+LOG_CONSOLE = neo_config["logs"]["console"]
+LOG_ACCOUNT_FILE = "neo_account.log"
+LOG_CIRCLE_FILE = "neo_circle.log"
+LOG_CONVERSATION_FILE = "neo_conversation.log"
+LOG_DEVICE_FILE = "neo_device.log"
+LOG_MEDIA_FILE = "neo_media.log"
+LOG_MESSAGE_FILE = "neo_message.log"
 LEVELS = {
     "DEBUG": logging.DEBUG,
     "INFO": logging.INFO,
--- utils/log.py
+++ utils/log.py
@@ -1,25 +1,28 @@
 import logging
-from config.log import LEVELS, LOG_LEVEL, LOG_ACTIVATE
+from os import path, makedirs
+from config.log import LEVELS, LOG_LEVEL, LOG_ACTIVATE, LOG_FOLDER, LOG_CONSOLE, LOG_FILE
 
 
 def logger_set(module, file=None):
+    if not path.exists(LOG_FOLDER):
+        makedirs(LOG_FOLDER)
     if file is None:
-        file = './neo_api.log'
+        file = 'neo_api.log'
     logger = logging.getLogger(module)
     logger.setLevel(LEVELS[LOG_LEVEL])
-
     if LOG_ACTIVATE is False:
         logging.disable(LEVELS.get("CRITICAL"))
     else:
-        # create a file handler
-        handler = logging.FileHandler(file)
-        handler.setLevel(LEVELS[LOG_LEVEL])
-
-        # create a logging format
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-
-        # add the handlers to the logger
-        logger.addHandler(handler)
+        if LOG_FILE is True:
+            file_handler = logging.FileHandler('%s/%s' % (LOG_FOLDER, file))
+            file_handler.setLevel(LEVELS[LOG_LEVEL])
+            file_handler.setFormatter(formatter)
+            logger.addHandler(file_handler)
+        if LOG_CONSOLE is True or (LOG_CONSOLE is False and LOG_FILE is False):
+            console_handler = logging.StreamHandler()
+            console_handler.setLevel(LEVELS[LOG_LEVEL])
+            console_handler.setFormatter(formatter)
+            logger.addHandler(console_handler)
     return logger
 
