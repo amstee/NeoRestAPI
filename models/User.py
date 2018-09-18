9f663d84f2d070a5a9f345ea03342f8217121131
Paul-Sirawit Kerebel <g.paul.kerebel@gmail.com>
2018-09-18 21:38:16 +0200

    fix facebook check

--- models/User.py
+++ models/User.py
@@ -262,7 +262,7 @@ class User(db.Model):
             "isOnline": self.is_online,
             "type": self.type,
             "hangout": False if self.hangout_space is None or len(self.hangout_space) == 0 else True,
-            "facebook": False if self.facebook_psid is None or self.facebook_psid <= 0 else True,
+            "facebook": False if self.facebook_psid is None or len(self.facebook_psid) == 0 else True,
             "circles": [link.get_content() for link in self.circle_link],
             "invites": [invite.get_content() for invite in self.circle_invite],
             "conversations": [link.get_simple_content() for link in self.conversation_links],
