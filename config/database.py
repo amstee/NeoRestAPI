93066a543b5220446df668d6d29bcb154f1e332a
Paul-Sirawit Kerebel <g.paul.kerebel@gmail.com>
2018-09-20 01:18:41 +0200

    add database config

--- config/database.py
+++ config/database.py
@@ -1,17 +1,24 @@
 from flask_sqlalchemy import SQLAlchemy
+import json
+
+with open('config.json') as data_file:
+    neo_config = json.load(data_file)
 
 POSTGRES = {
-    'user': '',
-    'pw': '',
-    'db': '',
-    'host': '',
-    'port': '',
+    'user': neo_config["database"]["postgresql"]["user"],
+    'pw': neo_config["database"]["postgresql"]["password"],
+    'db': neo_config["database"]["postgresql"]["database"],
+    'host': neo_config["database"]["postgresql"]["host"],
+    'port': neo_config["database"]["postgresql"]["port"]
 }
-URI_POSTGRES = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
-URI_SQLITE = 'sqlite:///database.sqlt'
-URI_TESTING = 'sqlite:///:memory:'
 
-URI_USED = URI_SQLITE
+TYPE = {
+    "POSTGRESQL": 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES,
+    "SQLITE": 'sqlite:///database.sqlt',
+    "TESTING": 'sqlite:///:memory:'
+}
+
+URI_USED = TYPE[neo_config["database"]["type"]]
 
 db = SQLAlchemy()
 
